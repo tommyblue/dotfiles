bc64b9a6f4a1e87bc3cbcabb5177c26196874922
Tommaso Visconti <tommaso.visconti@gmail.com>
2016-02-21 19:28:23 +0100

    Add force mode

--- install.sh
+++ install.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+if [ $# -ge 1 ] && [ $1 = "-f" ]; then
+    echo -e "=> Enabling FORCE MODE (existing dotfiles will be deleted) <="
+    FORCE=1
+else
+    FORCE=0
+fi
+
 for dotfile in `ls ./files/`; do
+    if [ $FORCE -eq 1 ]; then
+        rm $HOME/.$dotfile
+    fi
+
     if [ -e $HOME/.$dotfile ] || [ -L $HOME/.$dotfile ]; then
         echo -e ".$dotfile exists, skipping..."
     else
-        echo -e "ln -s ./$dotfile ~/.$dotfile"
-        ln -s ./$dotfile $HOME/.$dotfile
+        echo -e "ln -s `pwd`/files/$dotfile ~/.$dotfile"
+        ln -s `pwd`/files/$dotfile $HOME/.$dotfile
     fi
 done
